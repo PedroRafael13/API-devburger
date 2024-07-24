541ff1219a1f9e1b66d5e63c716ac522fb18d973
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-07-23 22:48:38 -0400

    >>

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,30 +1,29 @@
 import Sequelize, { Model } from "sequelize"
 import bcrypt from "bcrypt"
 class User extends Model {
-  static init(sequelize){
+  static init(sequelize) {
     super.init({
-      name : Sequelize.STRING,
-      email : Sequelize.STRING,
-      password : Sequelize.VIRTUAL,
-      password_hash : Sequelize.STRING,
-      admin : Sequelize.BOOLEAN,
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password: Sequelize.VIRTUAL,
+      password_hash: Sequelize.STRING,
+      admin: Sequelize.BOOLEAN,
     },
-    {
-      sequelize,
-    }
+      {
+        sequelize,
+      }
     )
 
     this.addHook('beforeSave', async (user) => {
-      if(user.password){
+      if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 10)
       }
     })
 
-
     return this
   }
 
-  async correctPassword(password){
+  async correctPassword(password) {
     return bcrypt.compare(password, this.password_hash)
   }
 }
