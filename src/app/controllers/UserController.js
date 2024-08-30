450853769a1d6accdfb2ff2942220d9b5225b10f
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-08-30 15:20:55 -0400

    atualizando o codigo

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,42 +2,43 @@ import { v4 } from "uuid"
 import User from "../models/User"
 import * as Yup from "yup"
 
-class UserController{
-  async store(request, response){
+class UserController {
+  async store(request, response) {
     const schema = Yup.object({
-      name : Yup.string().required(),
-      email : Yup.string().email().required(),
-      password : Yup.string().min(6).required(),
-      admin : Yup.boolean(),
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+      admin: Yup.boolean(),
     })
 
-    try{
-      schema.validateSync(request.body, { abortEarly : false })
+    try {
+      schema.validateSync(request.body, { abortEarly: false })
     } catch (err) {
-      return response.status(400).json({error: err.errors})
+      return response.status(400).json({ error: err.errors })
     }
-  
-    const {name, email, password, admin } = request.body 
+
+    const { name, email, password, admin } = request.body
 
     const userExists = await User.findOne({
-      where : {
+      where: {
         email,
       }
     })
 
-    if(userExists){
-      return response.status(400).json({error : "User already exists"})
+    if (userExists) {
+      return response.status(400).json({ error: "User already exists" })
     }
 
     const user = await User.create({
-      id : v4(),
-      name, 
+      id: v4(),
+      name,
       email,
       password,
+      admin,
     })
 
     return response.status(201).json({
-      user : user.id,
+      id: user.id,
       name,
       email,
       admin,
