d277f074f8fdf501d44f5f3310c30253dab82413
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-10-19 17:27:34 -0400

    fazend pequenas mudanças

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import app from './app';
-import 'dotenv/config';
+import app from './app'
+import 'dotenv/config'
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`O serve runnig at port 3001 ${port}!`))
