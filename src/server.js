e576ed0bfd38c7b268719eb21a22cab62555b3be
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-10-21 20:16:01 -0400

    .

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import app from './app'
+import app from './app'3
 import 'dotenv/config'
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`O serve runnig at port 3001 ${port}!`))
+app.listen(port, () => console.log(`O serve runnig at port 3001 ${port}!`)
